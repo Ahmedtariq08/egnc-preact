bda98a2908232ab719a427643ae1f5c264a6c67c
ahmed.tariq-at-101380220521 <ahmed.tariq@gosaas.io>
2023-09-27 15:32:07 +0500

    setup eslint and prettier complete

--- src/modules/admin-panel/attribute-mapping/attributeMappingStore.ts
+++ src/modules/admin-panel/attribute-mapping/attributeMappingStore.ts
@@ -1,59 +1,70 @@
-import { makeAutoObservable, runInAction } from 'mobx'
-import { Categories } from '../../../constants'
-import { type MappedAttribute, attributeMappingApis } from './attributeMappingService'
-import MutableArrayDataProvider = require('ojs/ojmutablearraydataprovider')
-import { store } from '../../../modules/store'
+import { makeAutoObservable, runInAction } from "mobx";
+import { Categories } from "../../../constants";
+import {
+    type MappedAttribute,
+    attributeMappingApis,
+} from "./attributeMappingService";
+import MutableArrayDataProvider = require("ojs/ojmutablearraydataprovider");
+import { store } from "../../../modules/store";
 
+type Category = typeof Categories.ITEM;
 export default class AttributeMappingStore {
-    attributes: MappedAttribute[] = []
-    loadingAttributes = false
-    selectedCategory = Categories.ITEM
-    selectedAttribute: MappedAttribute | undefined = undefined
-    popupHooks = { delete: false, edit: false }
+    attributes: MappedAttribute[] = [];
+    loadingAttributes = false;
+    selectedCategory: Category = Categories.ITEM;
+    selectedAttribute: MappedAttribute | undefined = undefined;
+    popupHooks = { delete: false, edit: false };
 
     constructor() {
-        makeAutoObservable(this)
+        makeAutoObservable(this);
     }
 
-    loadAttributes = async () => {
-        this.loadingAttributes = true
+    loadAttributes = async (): Promise<void> => {
+        this.loadingAttributes = true;
         try {
-            const response = await attributeMappingApis.getAttributes(this.selectedCategory.value)
-            console.log(response)
-            this.attributes = response
+            const response = await attributeMappingApis.getAttributes(
+                this.selectedCategory.value,
+            );
+            console.log(response);
+            this.attributes = response;
         } catch (error) {
-            store.commonStore.showNotification('error', `Unable to fetch attributes for ${this.selectedCategory.label}`)
+            store.commonStore.showNotification(
+                "error",
+                `Unable to fetch attributes for ${this.selectedCategory.label}`,
+            );
         } finally {
             runInAction(() => {
-                this.loadingAttributes = false
-            })
+                this.loadingAttributes = false;
+            });
         }
-    }
+    };
 
-    entitySelectionHandler = (category: any) => {
-        this.selectedCategory = category
-        this.loadAttributes()
-    }
+    entitySelectionHandler = (category: Category): void => {
+        this.selectedCategory = category;
+        void this.loadAttributes();
+    };
 
-    openPopup = (hook: keyof typeof this.popupHooks) => {
-        this.popupHooks = { ...this.popupHooks, [hook]: true }
-    }
+    openPopup = (hook: keyof typeof this.popupHooks): void => {
+        this.popupHooks = { ...this.popupHooks, [hook]: true };
+    };
 
-    closePopup = (hook: keyof typeof this.popupHooks) => {
-        this.popupHooks = { ...this.popupHooks, [hook]: false }
-    }
+    closePopup = (hook: keyof typeof this.popupHooks): void => {
+        this.popupHooks = { ...this.popupHooks, [hook]: false };
+    };
 
     // only show attribute1 - attribute 5
-    get attributesDp() {
-        const slicedAttributes = this.attributes.slice(0, 5)
-        return new MutableArrayDataProvider(slicedAttributes, { keyAttributes: 'attributeName' })
+    get attributesDp(): MutableArrayDataProvider<string, MappedAttribute> {
+        const slicedAttributes = this.attributes.slice(0, 5);
+        return new MutableArrayDataProvider(slicedAttributes, {
+            keyAttributes: "attributeName",
+        });
     }
 
-    get disableDelete() {
-        return !!this.selectedAttribute?.name
+    get disableDelete(): boolean {
+        return !(this.selectedAttribute?.name == null);
     }
 
-    get disableEdit() {
-        return !!this.selectedAttribute
+    get disableEdit(): boolean {
+        return !(this.selectedAttribute == null);
     }
 }
