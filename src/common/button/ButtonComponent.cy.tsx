4067d468afab972a886fec8cfcdf3f19e7a19d76
ahmed.tariq-at-101380220521 <ahmed.tariq@gosaas.io>
2023-10-04 13:13:52 +0500

    cleanup cypress

--- src/common/button/ButtonComponent.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { ButtonComponent } from "./ButtonComponent";
-
-describe("<ButtonComponent />", () => {
-    it("renders", () => {
-        // see: https://on.cypress.io/mounting-react
-        cy.mount(<ButtonComponent buttonTitle="test" />);
-    });
-});
